763aa4cff9729f612b70a284a631ebdd1906a71a
alessandro <alebeni02@gmail.com>
2021-03-05 11:09:29 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var lista = ['', 'Bianchi', 'Rossi', 'Perlari', 'Lodigiani', 'Mastrobattista', 'Verdi'];
+var lista = ['Bianchi', 'Rossi', 'Perlari', 'Lodigiani', 'Mastrobattista', 'Verdi'];
 var cognome = prompt("Dimmi il tuo cognome");
 console.log(cognome);
 if (isNaN(cognome)) {
@@ -6,7 +6,8 @@ if (isNaN(cognome)) {
   lista.push(cognomeupper);
   lista.sort();
 }
-console.log(lista.indexOf(cognomeupper, 1));
-for (var i = 1; i < 8; i++) {
+for (var i = 0; i < 7; i++) {
   document.getElementById('list').innerHTML += "<li>"+ lista[i] +"</li>";
 }
+var indexCognome = lista.indexOf((cognomeupper)+ 1);
+console.log(indexCognome);
